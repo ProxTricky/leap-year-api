d4e34868b491580466a844b4635aee20f560f339
Gurvan <gurvan@pincepoche.fr>
2024-10-30 14:44:41 +0100

    adding all files and changing gitignore

--- leapyear_api/leapyear_checker/views.py
+++ leapyear_api/leapyear_checker/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import LeapYearHistory
+from .utils import is_leap_year
+from django.utils import timezone
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def check_leap_year(request, year):
+    year = int(year)
+    result = is_leap_year(year)
+    # Sauvegarde dans l'historique
+    LeapYearHistory.objects.create(
+        endpoint="date",
+        input_data=str(year),
+        result={'year': year, 'is_leap': result}
+    )
+    return JsonResponse({'year': year, 'is_leap': result})
+
+@api_view(['GET'])
+def check_leap_year_range(request, start_year, end_year):
+    start_year, end_year = int(start_year), int(end_year)
+    leap_years = [year for year in range(start_year, end_year + 1) if is_leap_year(year)]
+    # Sauvegarde dans l'historique
+    LeapYearHistory.objects.create(
+        endpoint="range",
+        input_data=f"{start_year}-{end_year}",
+        result={'start_year': start_year, 'end_year': end_year, 'leap_years': leap_years}
+    )
+    return JsonResponse({'start_year': start_year, 'end_year': end_year, 'leap_years': leap_years})
+
+@api_view(['GET'])
+def history(request):
+    historys = LeapYearHistory.objects.all().values('endpoint', 'input_data', 'result', 'created_at')
+    formatted_historys = [
+        {
+            'endpoint': history['endpoint'],
+            'input_data': history['input_data'],
+            'result': history['result'],
+            'created_at': history['created_at'].strftime('%Y-%m-%d %H:%M:%S UTC')
+        }
+        for history in historys
+    ]
+    return JsonResponse({'history': formatted_historys})
